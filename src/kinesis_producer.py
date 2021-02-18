7fcc95be88eacf26effe53ed7d731be55606f54d
Jacob Alperin-Sheriff <jacobmas@gmail.com>
2021-02-18 14:27:13 -0500

    Added support for regular kinesis.

--- src/kinesis_producer.py
+++ src/kinesis_producer.py
@@ -8,10 +8,11 @@ import boto3
 
 log = logging.getLogger(__name__)
 
+
 class RecordAccumulator(object):
 
-    def __init__(self):
-        self.limit = 20
+    def __init__(self, limit=20):
+        self.limit = limit
         self.container = []
 
     def empty(self):
@@ -26,23 +27,32 @@ class RecordAccumulator(object):
 
 
 class KinesisProducer(object):
-    def __init__(self, api_name, region_name, stream_name):
+    def __init__(self, api_name, region_name, stream_name, partition_key='DEFAULT'):
+        self.api_name = api_name
         self.client = boto3.client(api_name, region_name=region_name)
         self.stream_name = stream_name
+        self.partition_key = partition_key
         self.accumulator = RecordAccumulator()
 
     def send(self, topic, data):
-       # print(f"data={data},type={type(data)}")
-        self.accumulator.append({
-            "Data": data.encode('utf-8'),
-        })
+        temp_record={"Data": data.encode('utf-8')}
+        if self.api_name=='kinesis':
+            temp_record['PartitionKey']=self.partition_key
+        self.accumulator.append(temp_record)
         if self.accumulator.full():
-            return self.client.put_record_batch(
-                Records=self.accumulator.empty(),
-                DeliveryStreamName=self.stream_name,
-            )
+           if self.api_name=='firehose':
+                return self.client.put_record_batch(
+                    Records=self.accumulator.empty(),
+                    DeliveryStreamName=self.stream_name)
+           elif self.api_name=='kinesis':
+                return self.client.put_records(
+                    Records=self.accumulator.empty(),
+                    StreamName=self.stream_name)
+           else:
+                raise Exception(f"Invalid api choice for KinesisProducer={self.api_name}")
         else:
             return True
+
 
 class StdoutProducer(object):
 
